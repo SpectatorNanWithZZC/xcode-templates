fab60aee139755285ead5f3f4d2f5eba625da2f1
spec <spectatornan@gmail.com>
2018-12-18 15:58:50 +0800

    modify

---
+++ spec/Source/ViewController and ViewModel.xctemplate/UIViewController/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import UIKit
+import Foundation
+import RxSwift
+import SNSwiftPackage
+
+
+class ___VARIABLE_sceneName___ViewController: SNBaseViewController {
+
+    typealias vm = ___VARIABLE_sceneName___ViewModel
+    var vmodel = vm()
+}
+
+extension ___VARIABLE_sceneName___ViewController {
+    
+    /// data load -- 数据加载  每次出现界面加载数据
+    override func loadData() {
+       
+    }
+    
+    /// setup view -- 加载视图
+    override func setupView() {
+        
+    }
+    
+    
+    ///  bind event -- 绑定事件处理
+    override func bindEvent() {
+        bbind()
+        
+        
+    }
+}
---
+++ spec/Source/ViewController and ViewModel.xctemplate/UIViewController/___FILEBASENAME___ViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+import SNSwiftPackage
+
+class ___VARIABLE_sceneName___ViewModel : SNBaseViewModel {
+
+
+}
+
+// MARK: - net  网络请求
+extension ___VARIABLE_sceneName___ViewModel {
+    
+    
+}
+
+// MARK: - parse 网络请求数据处理
+extension ___VARIABLE_sceneName___ViewModel {
+    
+    
+}
+
+// MARK: - map 网络请求数据转换
+extension ___VARIABLE_sceneName___ViewModel {
+    
+    
+}
+
+// MARK: - other 其他逻辑处理
+extension ___VARIABLE_sceneName___ViewModel {
+    
+    
+}
